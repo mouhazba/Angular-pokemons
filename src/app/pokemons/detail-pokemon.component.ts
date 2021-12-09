efe4db302c703f7d1330d670fc553fc4a7a6f470
mouhazba <bamahadiou@gmail.com>
2021-12-09 14:55:48 +0000

    service commit

--- src/app/pokemons/detail-pokemon.component.ts
+++ src/app/pokemons/detail-pokemon.component.ts
@@ -2,30 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
+import { PokemonService } from './pokemons.service';
  
 @Component({
     selector: 'list-pokemon',
     templateUrl: './detail-pokemon.component.html',
-    styleUrls: ['/app.component.css']
+    //styleUrls: ['/app.component.css']
 })
 export class DetailPokemonComponent implements OnInit {
  
-    pokemons!: Pokemon[] ;
     pokemon!: Pokemon;
     ids! :any;
  
-    constructor(private route: ActivatedRoute, private router: Router) {}
+    constructor(private route: ActivatedRoute, private router: Router, private pokemonService:PokemonService) {}
  
     ngOnInit():void {
-        this.pokemons = POKEMONS;
         
         this.ids = (this.route.snapshot.paramMap.get('id'));
-        
-         for (let i = 0; i < this.pokemons.length; i++) {
-            if (this.pokemons[i].id == this.ids) {
-                this.pokemon = this.pokemons[i];
-            }
-        } 
+        this.pokemon = this.pokemonService.getPokemon(this.ids)
     }
  
     goBack():void {
